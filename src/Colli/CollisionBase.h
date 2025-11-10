c8ff498c97392ccb013d3f35d7ed3185af28213e
OkayamaMayu <2309406@s.asojuku.ac.jp>
2025-11-10 18:38:42 +0900

    add：CollisionBaseクラスの作成

---
+++ src/Colli/CollisionBase.h
@@ -0,0 +1,41 @@
+#pragma once
+#include<functional>
+#include"../Model/Model.h"
+using namespace std;
+
+//当たり判定のタイプ
+enum COLLISION_TYPE {
+	TYPE_AABB,		//箱
+	TYPE_SPHERE,	//球
+	TYPE_LINE		//線分
+};
+
+//識別ID
+enum COLLISION_KIND {
+	KIND_STAGE	= 0,	//ステージブロック
+	KIND_PLAYER,		//プレイヤー
+	KIND_GOAL,			//ゴール
+	KIND_ENEMY	= 100,	//エネミー
+	KIND_ITEM	= 200,	//アイテム
+	KIND_BLOCK	= 300,	//配置ブロック
+};
+
+class CollisionBase {
+protected:
+	COLLISION_TYPE	m_CollisionType;				//当たり判定のタイプ
+	CModel*			m_Owner;						//オーナーのアドレス
+	bool			m_IsCollision;					//コリジョン有効フラグ
+	int				m_iKind;						//kindメンバー
+	function<void(CollisionBase)> m_OnHitCollback;	//ヒット時のコールバック関数
+
+public:
+	COLLISION_TYPE	GetCollisionType() { return m_CollisionType; }						//当たり判定のタイプを取得
+	CModel*			GetOwner() { return m_Owner; }										//オーナーのアドレスを取得
+	bool			IsCollision() { return m_IsCollision; }								//コリジョン有効フラグを取得
+	int				GetKind() { return m_iKind; }										//kindメンバーを取得
+
+	void SetOwner(CModel* set) { m_Owner = set; }										//オーナーのアドレスを設定
+	void SetIsCollision(bool set) { m_IsCollision = set; }								//コリジョン有効フラグを設定
+	void SetKind(int set) { m_iKind = set; }											//kindメンバーを設定
+	void SetOnHitCollback(function<void(CollisionBase)> set) { m_OnHitCollback = set; }	//ヒット時のコールバック関数を設定
+};
